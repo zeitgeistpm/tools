a155b6bcd738e68b44d6345cffce2c0cc7a69352
Logan Saether <X@logansaether.com>
2020-11-19 20:31:54 +0100

    Adds kinda not-so-good docs for now

--- packages/sdk/src/util/ipfs.ts
+++ packages/sdk/src/util/ipfs.ts
@@ -1,5 +1,6 @@
 import ipfsClient from "ipfs-http-client";
 
+/** Creates an IPFS client connected to the Zeitgeist-hosted node. */
 export const initIpfs = () => {
   return ipfsClient({
     url: "https://ipfs.zeitgeist.pm",
