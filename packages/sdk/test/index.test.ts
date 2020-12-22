14daf299e07e355b84514b8aec22ed40d2eb4b55
Logan Saether <X@logansaether.com>
2020-12-22 20:18:58 +0100

    Update to better types:

--- packages/sdk/test/index.test.ts
+++ packages/sdk/test/index.test.ts
@@ -1,7 +1,17 @@
+import { ApiPromise } from "@polkadot/api";
 import SDK from "../src/index";
 
 test('SDK can properly initialize', async () => {
   const sdk = await SDK.initialize();
   expect(sdk).toBeInstanceOf(SDK);
+  await sdk.api.disconnect();
+});
+
+test('Can use the API on the SDK', async () => {
+  const sdk = await SDK.initialize();
+  const api = sdk.api;
+  expect(api).toBeInstanceOf(ApiPromise);
+  const balRes = await api.query.system.account("5CS2Q1XbRR1eYnxeXUm8fqq6PfK3WLfwUvCpNvGsYAjKtsUC");
+  console.log(balRes.toHuman());
   await sdk.api.disconnect();
 });
