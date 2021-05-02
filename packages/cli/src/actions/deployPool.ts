4d155a0934f9a96cfd43f46944dc780798150a32
Logan Saether <X@logansaether.com>
2021-05-02 23:08:56 +0200

    Allow for weights to be passed in via CLI

--- packages/cli/src/actions/deployPool.ts
+++ packages/cli/src/actions/deployPool.ts
@@ -4,10 +4,11 @@ type Options = {
   endpoint: string;
   marketId: number;
   seed: string;
+  weights: string;
 };
 
 const deployPool = async (opts: Options): Promise<void> => {
-  const { endpoint, marketId, seed } = opts;
+  const { endpoint, marketId, seed, weights } = opts;
 
   const sdk = await SDK.initialize(endpoint);
 
@@ -15,11 +16,21 @@ const deployPool = async (opts: Options): Promise<void> => {
   console.log("Sending transaction from", signer.address);
 
   const market = await sdk.models.fetchMarketData(marketId);
+  const { outcomeAssets } = market;
 
-  const weights = Array(market.outcomeAssets.length+1)
-    .fill("1".concat("0".repeat(10)));
+  let wts = [];
+  if (weights) {
+    wts = weights.split(',');
+    if (wts.length !== outcomeAssets.length + 1) {
+      throw new Error(`Provided weights length must match assets length!\nWeights: ${wts.length}\nAssets: ${outcomeAssets.length + 1}`);
+    }
+  } else {
+    //default
+    wts = Array(outcomeAssets.length + 1)
+      .fill("1".concat("0".repeat(10)));
+  }
 
-  const res = await market.deploySwapPool(signer, weights);
+  const res = await market.deploySwapPool(signer, wts);
 
   console.log(res);
 
