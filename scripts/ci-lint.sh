4418524a0ed1011746df372709bd11474961b36d
Logan Saether <x@logansaether.com>
2021-05-07 02:51:25 +0200

    Add lint script

--- scripts/ci-lint.sh
+++ scripts/ci-lint.sh
@@ -1,5 +1,20 @@
 yarn cli:lint
 if [ $? -eq 1 ]
 then
+  echo "Need to fix cli!"
+  exit 1
+fi
+
+yarn sdk:lint
+if [ $? -eq 1 ]
+then
+  echo "Need to fix sdk!"
+  exit 1
+fi
+
+yarn type-defs:lint
+if [ $? -eq 1 ]
+then
+  echo "Need to fix type-defs!"
   exit 1
 fi
