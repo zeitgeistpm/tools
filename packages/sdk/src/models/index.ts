95da723074576ae597902dc539ae9b58fc67eeb3
Tvrtko Majstorovic <tvrtkomaj@gmail.com>
2021-06-04 21:30:19 +0200

    fixes resolved and reported outcome types

--- packages/sdk/src/models/index.ts
+++ packages/sdk/src/models/index.ts
@@ -239,6 +239,13 @@ export default class Models {
           });
         });
 
+    extendedMarket.report = market.report.isSome
+      ? market.report.value
+      : null;
+    extendedMarket.resolved_outcome = market.resolved_outcome.isSome
+      ? market.resolved_outcome.value.toNumber()
+      : null;
+
     Object.assign(extendedMarket, {
       ...data,
       marketId,
