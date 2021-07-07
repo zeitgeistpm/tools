75e0496e51df3c46cc04a2279f2f03af09d5fdbf
Logan Saether <x@logansaether.com>
2021-07-07 14:00:38 +0200

    Add new market metadata

--- packages/sdk/src/types/index.ts
+++ packages/sdk/src/types/index.ts
@@ -6,6 +6,34 @@ import {
 } from "@zeitgeistpm/types/dist/interfaces/predictionMarkets";
 import { Asset } from "@zeitgeistpm/types/dist/interfaces/index";
 
+// The possible fields available on the decoded market metadata.
+export type DecodedMarketMetadata = {
+  // The short name for the market, ex. 'TEAM 1 v.s TEAM 2'.
+  slug: string;
+  // The question for the market, capped at 160 chars.
+  question: string;
+  // The complete description of the market including all details.
+  description: string;
+  // If the market is categorical this field will contain the category data.
+  categories?: CategoryMetadata[];
+  // Tags for the market.
+  tags?: string[];
+  // If this field is present and contains content, the market is encrypted.
+  // An identifier is placed in this field to denote what key the client should
+  // use. E.g. this field might contain "us-gov:22313" to denote a decryption
+  // key attached to this identity will decrypt the data.
+  confidential_id?: string;
+  // The image for the market.
+  img?: string;
+};
+
+export type CategoryMetadata = {
+  name: string;
+  ticker?: string;
+  img?: string;
+  color?: string;
+};
+
 // Just a market identifier.
 export type MarketId = number;
 
