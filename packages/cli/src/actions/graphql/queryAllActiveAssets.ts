38625422787d8cf39545423b61fef28a9782c8f4
Tvrtko Majstorović <tvrtkomaj@gmail.com>
2022-02-20 09:11:41 +0100

    Change querying active assets to use subsquid only (#163)
    - also added search by market slug and pagination possibility

--- packages/cli/src/actions/graphql/queryAllActiveAssets.ts
+++ packages/cli/src/actions/graphql/queryAllActiveAssets.ts
@@ -3,14 +3,21 @@ import SDK from "@zeitgeistpm/sdk";
 type Options = {
   graphQlEndpoint: string;
   endpoint: string;
+  marketSlug?: string;
+  pageNumber?: number;
+  pageSize?: number;
 };
 
 const queryAllActiveAssets = async (opts: Options): Promise<void> => {
-  const { endpoint, graphQlEndpoint } = opts;
+  const { endpoint, graphQlEndpoint, marketSlug, pageNumber, pageSize } = opts;
 
   const sdk = await SDK.initialize(endpoint, { graphQlEndpoint });
+  let pagination: { pageSize: number; pageNumber: number };
+  if (pageNumber && pageSize) {
+    pagination = { pageSize, pageNumber };
+  }
 
-  const res = await sdk.models.queryAllActiveAssets();
+  const res = await sdk.models.queryAllActiveAssets(marketSlug, pagination);
 
   console.log(JSON.stringify(res, undefined, 4));
 };
