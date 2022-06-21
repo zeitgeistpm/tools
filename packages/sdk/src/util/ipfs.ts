88d8c612de39222c12e92b9d4cf66fd3c6f63ae2
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-06-21 09:35:57 +0530

    Remove initIpfs util

--- packages/sdk/src/util/ipfs.ts
+++
@@ -1,9 +0,0 @@
-import ipfsClient from "ipfs-http-client";
-
-export const initIpfs = (
-  ipfsClientUrl = "https://ipfs.zeitgeist.pm"
-): ReturnType<typeof ipfsClient> => {
-  return ipfsClient({
-    url: ipfsClientUrl,
-  });
-};
