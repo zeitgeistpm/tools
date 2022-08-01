0a66dce2f941b62789070b3b9bcdaa109e7d3224
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-08-01 08:45:59 +0530

    Sync changes on cli handlers

--- packages/cli/src/actions/deployPool.ts
+++ packages/cli/src/actions/deployPool.ts
@@ -4,12 +4,13 @@ type Options = {
   endpoint: string;
   marketId: number;
   seed: string;
+  swapFee: string;
   amount: string;
   weights: string;
 };
 
 const deployPool = async (opts: Options): Promise<void> => {
-  const { endpoint, marketId, seed, weights, amount } = opts;
+  const { endpoint, marketId, seed, weights, amount, swapFee } = opts;
 
   const sdk = await SDK.initialize(endpoint);
 
@@ -25,21 +26,20 @@ const deployPool = async (opts: Options): Promise<void> => {
   let wts = [];
   if (weights) {
     wts = weights.split(",");
-    if (wts.length !== outcomeAssets.length + 1) {
-      throw new Error(
-        `Provided weights length must match assets length!\nWeights: ${
-          wts.length
-        }\nAssets: ${outcomeAssets.length + 1}`
-      );
-    }
   } else {
     //default
     // do not exceed: pub const MaxWeight: Balance = 50 * BASE;
     // (See: /zeitgeist/runtime/src/lib.rs )
-    wts = Array(outcomeAssets.length + 1).fill("1".concat("0".repeat(10)));
+    wts = Array(outcomeAssets.length).fill("1".concat("0".repeat(10)));
   }
 
-  const poolId = await market.deploySwapPool(signer, amount, wts, false);
+  const poolId = await market.deploySwapPool(
+    signer,
+    swapFee,
+    amount,
+    wts,
+    false
+  );
   if (poolId && poolId.length > 0) {
     console.log(`\x1b[36m%s\x1b[0m`, `\nDeployPool successful!`);
   } else {
