9d1e2cabe17c600abd3bcac5100d83941a3e6c02
Nikhil Saboo <ns.nikhilsaboo@gmail.com>
2022-11-28 19:29:31 +0530

    Allow reason to be supplied via cli

--- packages/cli/src/actions/rejectMarket.ts
+++ packages/cli/src/actions/rejectMarket.ts
@@ -5,19 +5,24 @@ type Options = {
   marketId: string;
   outcome: string;
   seed: string;
+  reason: string;
 };
 
 const rejectMarket = async (opts: Options): Promise<void> => {
-  const { endpoint, marketId, seed } = opts;
+  const { endpoint, marketId, reason, seed } = opts;
 
   const sdk = await SDK.initialize(endpoint);
 
   const signer = util.signerFromSeed(seed);
 
   const market = await sdk.models.fetchMarketData(Number(marketId));
-  const res = await market.reject(signer, false);
+  const res = await market.reject(signer, reason, false);
 
-  console.log(res);
+  if (res && res.length > 0) {
+    console.log(`\x1b[36m%s\x1b[0m`, `\nRejectMarket successful!`);
+  } else {
+    console.log(`\x1b[36m%s\x1b[0m`, `\nRejectMarket failed!`);
+  }
   return;
 };
 
